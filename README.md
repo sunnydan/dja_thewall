d877d06289e90c808f0782086507632f44e55389
sunnydan <danielhilla@gmail.com>
2017-09-18 13:29:28 -0400

    updated functionality with ability to view, update, and delete users for semirestfulusers assignment

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ Allow the user to delete his/her own messages.
 
 Extra Credit II (optional)
 Allow the user to delete his/her own message but only if the message was made in the last 30 minutes.
+
+Assignment: Semi-Restful Users
+Create a web app that can handle all of the CRUD operations (create, read, update and destroy) for a table. Use your friends database for the following assignment.
+
+But first, what does REST mean?
+It's very common for a web application to provide the user interface for creating, reading, updating, or destroying a 'resource' (a table). For example, imagine you want to build a web application that allows the user to create/read/update/destroy users. There are many ways that you can build web applications like this. For example, you could have resources called users, products, pd (short for products) and so forth. You could also have different methods that essentially do the same thing. So, to display user information for user id 1, you could have the URL 'users/1' provide this info or 'users/show/1' or 'users/show_info/1' or 'users/display/1', etc.
+
+Since many web applications perform CRUD operations, you can imagine how confusing this could get if everyone followed different conventions for creating routing and method names for these operations.
+
+A REST or RESTful route is simply a set of route naming conventions that the industry has agreed to follow in order to make it easier to send requests to APIs. It's up to you whether you also follow these rules/conventions but we strongly encourage you to get familiar with how the following rules for RESTful routing, as you may be making requests to, or someday creating your own, API.
+
+Right now with Flask, it's not quite possible for you to do the full RESTful architecture, so the exercise below is to help you get somewhat familiar with RESTful routes. Later when you get into other stacks (such as MEAN or Rails), you'll already be somewhat familiar with REST concepts.
+
+Follow the instructions in the wireframe below to build this application in Flask.
--- server.py
+++ server.py
@@ -15,15 +15,12 @@ def index():
     session['previous_last_name'] = ""
     if 'logged_in' not in session:
         session['logged_in'] = False
-    if session['logged_in']:
-        print "User ID: " + str(session['logged_in']['id'])
+    if session['logged_in']:       
         dbusers = mysql.query_db("SELECT * FROM users")
         dbmessages = mysql.query_db("SELECT * FROM messages")
         dbcomments = mysql.query_db("SELECT * FROM comments")
         tmessages = []
         for dbmessage in dbmessages:
-            print "Message Author ID: " + str(dbmessage['user_id'])
-            print str(dbmessage['user_id'] == session['logged_in']['id'])
             tmessage = {}
             for dbuser in dbusers:
                 if dbuser['id'] == dbmessage['user_id']:
@@ -33,7 +30,7 @@ def index():
             else:
                 tmessage['timestamp'] = dbmessage['created_at']
             tmessage['message'] = dbmessage['message']
-            tmessage['id'] = dbmessage['user_id']
+            tmessage['id'] = dbmessage['id']
             if dbmessage['user_id'] == session['logged_in']['id']:
                 tmessage['deleteable'] = True
             else:
@@ -41,7 +38,6 @@ def index():
             tmessage['comments'] = []
             for dbcomment in dbcomments:
                 if dbcomment['message_id'] == dbmessage['id']:
-                    print "Comment Author ID: " + str(dbcomment['user_id'])
                     tcomment = {}
                     for cdbuser in dbusers:
                         if cdbuser['id'] == dbcomment['user_id']:
@@ -51,14 +47,14 @@ def index():
                     else:
                         tcomment['timestamp'] = dbcomment['created_at']
                     tcomment['comment'] = dbcomment['comment']
-                    tcomment['id'] = dbcomment['user_id']
+                    tcomment['id'] = dbcomment['id']
                     if dbcomment['user_id'] == session['logged_in']['id']:
                         tcomment['deleteable'] = True
                     else:
                         tcomment['deleteable'] = False
                     tmessage['comments'].append(tcomment)
             tmessages.append(tmessage)
-        return render_template("index.html", users=mysql.query_db("SELECT * FROM users"), logged_in=True, email=session['logged_in']['email'], messages=tmessages)
+        return render_template("index.html", users=mysql.query_db("SELECT * FROM users"), logged_in=True, email=session['logged_in']['email'], messages=tmessages, user = session['logged_in'])
     return render_template("index.html", users=mysql.query_db("SELECT * FROM users"))
 
 @app.route('/register')
@@ -96,7 +92,7 @@ def processregister():
         session['previous_email'] = ""
         session['previous_first_name'] = ""
         session['previous_last_name'] = ""
-        query = "INSERT INTO users (email, first_name, last_name, password) VALUES (:email, :first_name, :last_name, :password)" 
+        query = "INSERT INTO users (email, first_name, last_name, password, created_at, updated_at) VALUES (:email, :first_name, :last_name, :password, NOW(), NOW())" 
         data = {
             'email': request.form['email'], 
             'first_name': request.form['first_name'], 
@@ -108,12 +104,59 @@ def processregister():
         for user in users:
             if user['email'] == request.form['email']:
                 session['logged_in'] = user 
-        return redirect('/userpage')
+        return redirect('/userpage/'+str(user['id']))
     else:
         session['previous_email'] = request.form['email']
         session['previous_first_name'] = request.form['first_name']
         session['previous_last_name'] = request.form['last_name']
-        return redirect('/register')    
+        return redirect('/register') 
+
+@app.route('/updateuser/<userid>')   
+def updateuser(userid):
+    try:
+        user = mysql.query_db("SELECT * FROM users WHERE id = "+str(userid))[0]
+    except IndexError:
+        return redirect('/')
+    return render_template("updateuser.html", user=user);
+
+@app.route('/processupdateuser/<userid>', methods=['POST'])
+def processupdateuser(userid):
+    try:
+        user = mysql.query_db("SELECT * FROM users WHERE id = "+str(userid))[0]
+    except IndexError:
+        return redirect('/')
+    valid = True
+    if len(request.form['first_name']) < 2:
+        flash("*First Name cannot be empty.")
+        valid = False
+    if len(request.form['last_name']) < 2:
+        flash("*Last Name cannot be empty.")
+        valid = False
+    if user['password'] != md5.new(request.form['password']).hexdigest():
+        flash("*Current Password is incorrect")
+        valid = False
+    if not PASSWORD_REGEX.match(request.form['new_password']):
+        flash("*New Password must contain at least 1 capital letter and at least 1 number, and be at least 8 characters long.")
+    if request.form['new_password'] != request.form['confirm_new_password']:
+        flash("*New Password inputs must match.")
+        valid = False
+    if not EMAIL_REGEX.match(request.form['email']):
+        flash("*Invalid Email Address.")
+        valid = False
+    print user['password']
+    print request.form['password']
+    print md5.new(request.form['password']).hexdigest()
+    if valid:
+        query = "UPDATE users SET email = '"
+        query += request.form['email'] + "', first_name = '"
+        query += request.form['first_name'] + "', last_name = '"
+        query += request.form['last_name'] + "', password = '"
+        query += md5.new(request.form['new_password']).hexdigest() + "', updated_at = NOW() WHERE id = "
+        query += str(userid)
+        mysql.query_db(query)
+        return redirect('/userpage/'+str(user['id']))
+    else:
+        return redirect('/updateuser/'+str(user['id']))
 
 @app.route('/login')
 def login():
@@ -123,37 +166,55 @@ def login():
 
 @app.route('/processlogin', methods=["POST"])
 def processlogin():
-    users=mysql.query_db("SELECT * FROM users")
-    for user in users:
+    dbusers=mysql.query_db("SELECT * FROM users")
+    for user in dbusers:
         if user['email'] == request.form['email'] and user['password'] == md5.new(request.form['password']).hexdigest():
             session['previous_email'] = ""
             session['logged_in'] = user
-            return redirect('/userpage')
+            return redirect('/userpage/'+str(user['id']))
     flash("*Email or password is incorrect.")
     session['previous_email'] = request.form['email']
     return redirect('/login')
 
 
-@app.route('/delete')
-def delete():
-    return render_template("delete.html")
+@app.route('/deleteuser/<userid>')
+def delete(userid):
+    return render_template("delete.html", id=userid)
 
-@app.route('/deleteconfirm', methods=['POST'])
-def deleteconfirm():
-    users=mysql.query_db("SELECT * FROM users")
-    for user in users:
-        if user['password'] == md5.new(request.form['password']).hexdigest():
-            session['logged_in'] = False 
-            mysql.query_db("DELETE FROM users WHERE id =" + str(user['id']))
-            return redirect('/')
+@app.route('/deleteconfirm/<userid>', methods=['POST'])
+def deleteconfirm(userid):
+    try:
+        user = mysql.query_db("SELECT * FROM users WHERE id ="+str(userid))[0]
+    except IndexError:
+        return redirect('/')
+    if user['password'] == md5.new(request.form['password']).hexdigest():
+        messages = mysql.query_db("SELECT * FROM messages WHERE user_id = " + str(user['id']))
+        for message in messages:
+            mysql.query_db("DELETE FROM comments WHERE message_id = " + str(message['id']))
+        mysql.query_db("DELETE FROM messages WHERE user_id =" + str(user['id']))
+        if session['logged_in']['id'] == user['id']:
+            session['logged_in'] = False
+        mysql.query_db("DELETE FROM users WHERE id =" + str(user['id']))
+        return redirect('/')
     flash("*Password is incorrect.")
-    return redirect('/login')
+    return redirect('/deleteconfirm/'+str(userid))
 
-@app.route('/userpage')
-def userpage():
-    if "logged_in" not in session or not session['logged_in']:
-        return redirect("/")
-    return render_template("userpage.html", email=session['logged_in']['email'], first_name=session['logged_in']['first_name'], last_name=session['logged_in']['last_name'], id=session['logged_in']['id'])
+@app.route('/users')
+def users():
+    dbusers = mysql.query_db("SELECT * FROM users")
+    return render_template("users.html", users = dbusers)
+
+@app.route('/userpage/<userid>')
+def userpage(userid):
+    try:
+        dbuser = mysql.query_db("SELECT * FROM users WHERE id =" + str(userid))[0]
+    except IndexError:
+        return redirect('/')
+    if session['logged_in']['id'] == dbuser['id']:
+        p = True
+    else:
+        p = False
+    return render_template("userpage.html", user = dbuser, permission = p)
 
 @app.route('/logout')
 def logout():
@@ -181,10 +242,15 @@ def processtext():
 @app.route('/deletetext', methods=['POST'])
 def deletetext():
     if request.form['type'] == "message":
-        query = "DELETE FROM messages WHERE id = " + request.form['id']
+        mysql.query_db("DELETE FROM comments WHERE message_id = " + str(request.form['id']))
+        mysql.query_db("DELETE FROM messages WHERE id = " + str(request.form['id']))
     else:
-        query = "DELETE FROM comments WHERE id = " + request.form['id']
-    mysql.query_db(query)
+        #mysql.query_db("DELETE FROM comments WHERE id = " + str(request.form['id']))
+        query = "DELETE FROM comments WHERE id = :id"
+        data = {
+            'id': request.form['id']
+        }
+        mysql.query_db(query, data)
     return redirect("/")
 
 app.run(debug=True)
